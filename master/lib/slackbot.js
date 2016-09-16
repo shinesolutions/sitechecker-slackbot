2482528f71f213de9aeb1dc2e999342187750b9e
Cliffano Subagio <cliffano@gmail.com>
2016-09-16 23:22:56 +1000

    Remove unused module.

--- master/lib/slackbot.js
+++ master/lib/slackbot.js
@@ -1,9 +1,5 @@
 'use strict';
-const isSiteUp = require('is-site-up');
 
-isSiteUp('www.vivekpoddar.com').then(isUp => {
-  console.log(isUp); // true
-});
 module.exports.handle = (event, context, cb) => cb(null,
-  { message: 'SlackBot ping', event }
+  { message: 'SlackBot ping ' + context.url, event }
 );
