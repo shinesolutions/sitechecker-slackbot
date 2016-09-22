98d24c1db89fd9ae9a8ce6151ab3f7cba4546d1b
Cliffano Subagio <cliffano@gmail.com>
2016-09-22 21:52:12 +1000

    Slack Outgoing Webhook uses POST method, switching to event body instead as data field.

--- master/lib/lambda.js
+++ master/lib/lambda.js
@@ -1,3 +1,3 @@
 const bot = require('./bot');
 
-module.exports.handle = (event, context, cb) => bot.interact(event.query, cb);
+module.exports.handle = (event, context, cb) => bot.interact(event.body, cb);
