11fcbef4f8068bbcc7a19be4442d29c555aef7c9
Cliffano Subagio <cliffano@gmail.com>
2016-09-23 14:31:49 +1000

    Bypass error callback, pass an error payload instead in order to not block async results.

--- worker/lib/sitechecker.js
+++ worker/lib/sitechecker.js
@@ -13,7 +13,21 @@ function check(url, cb) {
       }
     }
   };
-  req.request(config.method, url, opts, cb);
+  req.request(config.method, url, opts, process(url, cb));
+}
+
+function process(url, cb) {
+  return function (err, result) {
+    if (err) {
+      // bypass error so it doesn't block the results from other regions
+      console.error('Unable to check URL due to error %s', err.message);
+      cb(null, { "status": "error", "message": err.message });
+    } else {
+      console.log('Successfully checked URL %s', url);
+      delete result.body; // remove body to reduce payload size
+      cb(null, result);
+    }
+  };
 }
 
 exports.check = check;
